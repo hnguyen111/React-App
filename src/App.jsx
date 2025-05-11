d01c53d36e179709214ae5052f298ba34ac0a2be
Hantha Nguyen <hantha.nguyen72@gmail.com>
2025-05-12 02:55:06 +1000

    Added Product component, with props and states

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Product from './components/Product/Product'
 
 function App() {
-  const [count, setCount] = useState(0)
+	// const randomMonth = Math.floor(Math.random() * 12 + 1);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	// return <div>
+	// 	<h2>Random month {randomMonth}</h2>
+	// 	<h1>React App</h1>
+	// </div>
+
+	const products = [
+		{
+			title: "Vase",
+			price: 35,
+			image: "https://images.unsplash.com/photo-1612196808214-b8e1d6145a8c?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+			description: "Ceramic vase in white"
+		},
+		{
+			title: "Trumpet",
+			price: 260,
+			image: "https://images.unsplash.com/photo-1573871666457-7c7329118cf9?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+			description: "Brass-colored trumpet"
+		}
+	]
+	return <section>
+		{products.map((product, index) => {
+			return <Product 
+				key={index}
+				title={product.title}
+				price={product.price}
+				image={product.image}
+				description={product.description}
+			/>
+		})}
+	</section>
 }
 
 export default App
---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import styles from './Product.module.css';
+
+const Product = (props) => {
+    const [title, setTitle] = useState(props.title);
+    const productOnClickHandler = () => {
+        console.log('Unknown');
+        setTitle('Unknown');
+    }
+    return (
+        <figure onClick={productOnClickHandler} className={styles.product}>
+            <img width="250px" src={props.image} alt="" />
+            <figcaption>
+                <h2>{title}</h2>
+                <h3>${props.price}</h3>
+                <p>{props.description}</p>
+                <button>Buy Now</button>
+            </figcaption>
+        </figure>
+    )
+}
+
+export default Product;
